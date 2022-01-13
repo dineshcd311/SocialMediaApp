c8ccf54ea2745e9341f765f5033eaefad063709c
Dinesh Chandran <dineshchandran311@gmail.com>
2022-01-13 09:41:39 +0530

    Setup

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
@@ -19,7 +21,24 @@ app.get("/", (req, res) => {
   res.send("APP IS RUNNING");
 });
 
-const PORT = process.env.PORT || 5000;
+app.use(function (req, res, next) {
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts("html")) {
+    res.render("404", { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts("json")) {
+    res.send({ error: "Not found" });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type("txt").send("Not found");
+});
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
